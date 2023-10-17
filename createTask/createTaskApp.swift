371259fb6aa4ee0ce1e9a5c814193fd9fa83832c
LostFox <51424369+LosttFox@users.noreply.github.com>
2023-10-17 13:51:47 -0600

    created repo, added gitignore

---
+++ createTask/createTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  createTaskApp.swift
+//  createTask
+//
+//  Created by Park, Andrew on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct createTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
