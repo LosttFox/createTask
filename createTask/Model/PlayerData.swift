e665a1fc8542b73e1c23f6e686a54ef3a4977faf
LostFox <51424369+LosttFox@users.noreply.github.com>
2023-12-06 12:02:06 -0700

    created player data model

---
+++ createTask/Model/PlayerData.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerData.swift
+//  createTask
+//
+//  Created by Park, Andrew on 11/20/23.
+//
+
+import Foundation
+
+struct PlayerData : Decodable
+{
+    let success : Bool
+    let player : String
+}
--- createTask/Resources/CreateTaskApp.swift
+++ createTask/Resources/CreateTaskApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct createTaskApp: App {
+struct CreateTaskApp: App {
     var body: some Scene {
         WindowGroup {
             TaskView()
